1b56a3e15f290c6e3cc56f80864e0c32ad31695c
Such1337Hax0r <Sgkadesh@icloud.com>
2015-11-21 16:31:16 -0500

    Added Files

---
+++ QDecoder.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class QDecoder {
+	public static void main(String[] Args){
+		boolean B = true;
+		Scanner S = new Scanner(System.in);
+		
+		while(B){
+			System.out.println("Enter A Binary String");
+			String J = "";
+			J = S.nextLine();
+			String[] NS = new String[J.split(" ").length];
+			if(J.contains(" ")){
+				NS = J.split(" ");
+				for(int X = 0; X < NS.length; X++){
+					System.out.print(Character.toChars(Integer.parseInt(NS[X], 2) -1));
+				}
+				
+				
+			}
+			System.out.println("");
+			System.out.println("More? (Y/N)");
+			if(S.nextLine().equalsIgnoreCase("N")){
+				B = false;
+			}
+		}
+	}
+}
